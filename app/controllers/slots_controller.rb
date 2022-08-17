797cc7050020ce492b009dff6a9980ec91a5ea12
Shwetambari Helwade <shweta.helwade@gmail.com>
2022-08-17 21:04:20 +0530

    Added APIs for index, show, create slot and on create, generate associated slot collections

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,2 +1,41 @@
 class SlotsController < ApplicationController
+    before_action :set_slot, only: [:show, :destroy]
+
+    # GET /slots
+    def index
+        @slots = Slot.all
+        render json: @slots
+    end
+
+    # GET /slots/:id
+    def show
+        render json: @slot
+    end
+
+    # POST /slots
+    def create
+        @slot = Slot.new(slot_params)
+
+        if @slot.save
+            render json: @slot, status: :created, location: @slot
+        else
+            render json: @slot.errors, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE /slots/1
+    def destroy
+        @slot.destroy
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_slot
+        @slot = Slot.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def slot_params
+        params.require(:slot).permit(:total_capacity, :start_time, :end_time)
+    end
 end
--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,5 +1,6 @@
 class Slot < ApplicationRecord
     has_many :slot_collections
+    SLOT_DURATION = 15 #minutes
     SLOT_MINUTES_OPTIONS = [0, 15, 30, 45]
 
     validates :total_capacity, :start_time, :end_time, presence: true
@@ -7,6 +8,38 @@ class Slot < ApplicationRecord
     validate :start_time_must_be_before_end_time
     validate :date_cannot_be_in_the_past
     validate :date_minutes
+
+    after_create :generate_slot_collections
+
+    def generate_slot_collections
+        capacities_to_be_alloted = total_capacity
+        slot_collection_start_time = start_time
+
+        available_slots_in_selected_duration.downto(1) do |available_slot|
+            slot_capacity = capacities_to_be_alloted / available_slot
+            slot_collection_end_time = slot_collection_start_time + SLOT_DURATION.minutes
+
+            # create associated slot collection
+            slot_collections.create(capacity: slot_capacity,
+             start_time: slot_collection_start_time,
+             end_time: slot_collection_end_time)
+
+            slot_collection_start_time = slot_collection_end_time
+            capacities_to_be_alloted -= slot_capacity
+        end
+    end
+
+    def available_slots_in_selected_duration
+        (time_duration_in_minutes / SLOT_DURATION).to_i
+    end
+
+    def time_duration_in_minutes
+        time_duration / 1.minutes
+    end
+
+    def time_duration
+        end_time - start_time
+    end
 
     private
 
